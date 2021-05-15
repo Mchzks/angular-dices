0f38b26e376b5d5e570e146c1aa5f66b7e193694
Danielesk1 <art.copor@yahoo.com>
2021-05-15 04:06:34 -0500

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DicesApp';
+
+  leftDice = '../assets/images/dice1.png';
+  rightDice = '../assets/images/dice1.png';
+
+  leftValue: number = 0;
+  rightValue: number = 1;
+
+  roll(): void {
+    this.leftValue = Math.round(Math.random() * 5) + 1;
+    this.rightValue = Math.round(Math.random() * 5) + 1;
+
+    this.leftDice = '../assets/images/dice' + this.leftValue + '.png';
+    this.rightDice = '../assets/images/dice' + this.rightValue + '.png';
+  }
 }
